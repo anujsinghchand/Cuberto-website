11d84fd67059b6e8c610d0850ef5ddeab9265514
anujsinghchand <84612089+anujsinghchand@users.noreply.github.com>
2025-06-17 10:34:19 +0530

    Add files via upload
    Clone of an Award wining website.

---
+++ cuberto.js
@@ -0,0 +1,45 @@
+// function factorial(n) {
+//   if (n == 1) {
+//     return 1;
+//   }
+//   return n * factorial(n - 1);
+// }
+
+// console.log(factorial(10));
+
+// function factorial(n) {
+//   if (n == 0 || n == 1) {
+//     return 1;
+//   } else {
+//     fact = 1;
+//     for (i = n; i > 1; i--) {
+//       var fact = fact * i;
+//     }
+//     return fact;
+//   }
+// }
+
+// console.log(factorial(10));
+
+//palindrome
+
+// function palindrome(input) {
+//   const str = input.toString();
+//   let newstr = str.split("").reverse().join("");
+//   return str === newstr;
+// }
+
+// console.log(palindrome("level"));
+
+// function longestword(input) {
+//   let newvalue = input.split(" ");
+//   let longest = "";
+//   newvalue.forEach((elem) => {
+//     if (elem.length > longest.length) {
+//       longest = elem;
+//     }
+//   });
+//   console.log(longest);
+// }
+// longestword("hii my name is datavalue boss and i am heroesofthepast");
+
